efa3847c46ec50df9cc35d8f928f64eb89f1a494
RickGelhausen <rick.gelhausen@gmail.com>
2023-09-28 17:00:59 +0200

    fixes

--- .github/scripts/build-pages.sh
+++ .github/scripts/build-pages.sh
@@ -4,16 +4,18 @@ set -x
 # Install Miniforge
 wget -qO- https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-Linux-x86_64.sh | bash
 source ~/miniforge3/bin/activate
+export PATH="$HOME/miniforge3/bin:$PATH"
 
 # Install Mamba
 conda install mamba -c conda-forge
 
 mamba create -n rmarkdown
-conda activate rmarkdown
+source ~/miniforge3/bin/activate rmarkdown
 
 mamba install -c conda-forge r-base r-rmarkdown r-flextable r-officer pandoc
 
-pwd ls -lah
+pwd
+ls -lah
 
 #######################
 # BUILD DOCUMENTATION #
